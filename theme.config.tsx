95ecd02aeb79e42bc86e89ea0e976eea2209842d
isasharafdin <isasharafdin@gmail.com>
2024-02-13 22:46:22 +0300

    added Dynamic Tags Based on Page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,29 @@
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://docs.yonode.org" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "Yonode"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "The Node.js Toolkit for Rapid Development."}
+        />
+      </>
+    );
+  },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s - Yonode'
-    }
+      titleTemplate: "%s - Yonode",
+    };
   },
   logo: "Yonode",
   feedback: {
