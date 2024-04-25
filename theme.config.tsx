19950ed131055d989ef86ebc2799772afaf459ac
شرف الدين <83120892+isasharafdin@users.noreply.github.com>
2024-04-25 15:39:27 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -39,7 +39,7 @@ const config: DocsThemeConfig = {
     key: "v0.5.5-beta",
     text: (
       <a href="https://github.com/sharafdin/yonode/releases" target="_blank">
-        Yonode will launch on Mar 11, 2024.
+        Yonode has officially launched!
       </a>
     ),
   },
